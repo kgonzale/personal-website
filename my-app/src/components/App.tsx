682bc94b45a2859741e49fc60c2ed095109dfce8
unknown <kgonzale@cbu.edu>
2019-05-17 14:04:50 -0500

    aids

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const App = () => {
-    return <h1>kevin</h1>
-}
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1 style={{ fontFamily: "Roboto" }} className="text-6xl text-center">
+        Kevin Gonzalez
+      </h1>
+      <div>
+        <a className="social" href="https://github.com/kgonzale" title="Github">
+          <i className="fab fa-github" />
+        </a>
+        <a className="social" href="https://www.linkedin.com/in/gonzalek/" title="LinkedIn">
+          <i className="fab fa-linkedin" />
+        </a>
+        <a className="social" href="mailto:kevin@kgonzale.com" title="E-Mail">
+          <i className="far fa-envelope" />
+        </a>
+        <a className="social" href="https://docdro.id/OjNi9Wg" title="Resume">
+          <i className="far fa-file" />
+        </a>
+      </div>
+    </React.Fragment>
+  );
+};
 
-export default App; 
+export default App;
